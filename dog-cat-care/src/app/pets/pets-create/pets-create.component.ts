552ccd1cafce701f8ebdf1e9e2bb9934dc40bc2e
Eduardo Lourenco <eduardo.lourenco2310@gmail.com>
2020-10-18 20:20:45 -0300

    form create pet

--- dog-cat-care/src/app/pets/pets-create/pets-create.component.ts
+++ dog-cat-care/src/app/pets/pets-create/pets-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {MustMatch} from "../../util/validator";
+import {Router} from "@angular/router";
+import {UtilService} from "../../util/util.service";
 
 @Component({
   selector: 'app-pets-create',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PetsCreateComponent implements OnInit {
 
-  constructor() { }
+  formRegister: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private utilService: UtilService
+  ) { }
 
   ngOnInit(): void {
+    this.formRegister = this.formBuilder.group({
+      name: new FormControl('', Validators.required),
+      tipo: new FormControl('', Validators.required)
+    });
+  }
+
+  toggleRegister() {
+    if(this.formRegister.valid){
+      this.router.navigate(['/home/pets']);
+    } else {
+      this.utilService.showMessage('Preencha todos os campos',true);
+    }
   }
 
 }
